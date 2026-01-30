b01e1bcbdc1c7eb85cd5974867b9642d4410a685
Parth Arora <partaror@qti.qualcomm.com>
2026-01-30 00:48:52 -0800

    Add primitive support for link-time memory reports
    This commit adds primitive support for link-time memory reports.
    The memory report contains memory usage information for each (most!)
    timer that we have in the codebase (eld::RegisterTimer).
    
    The main motivation for link-time memory reports is to help find out which
    linker areas to focus on for reducing the link memory footprint.
    
    The memory usage information contains the current resident set size,
    the resident set size change in this timer, and the peak resident set
    size seen so far. All these information are computed by parsing the
    virtual file '/proc/self/status'. As expected, this solution would not
    work for windows and thus this feature is only available for
    eld-on-linux. The virtual file '/proc/self/status' may be represented
    slightly different across different linux distributions so we might see
    some issues in different linux distributions. Thus, this feature should
    be considered experimental for now.
    
    Memory usage information is not recorded for timers that are created a
    large number of times, for example, VisitSymbol and VisitSections. This
    is because each read to the virtual file '/proc/self/status' is a
    system call and making large number of this system call can take
    considerable time. This is fine because we can always improve/rearrange
    timers such that we get the memory information that we need.
    
    Signed-off-by: Parth Arora <partaror@qti.qualcomm.com>

--- lib/Support/RegisterTimer.cpp
+++ lib/Support/RegisterTimer.cpp
@@ -9,6 +9,7 @@
 #include "eld/Support/MsgHandling.h"
 #include "llvm/Support/Format.h"
 #include "llvm/Support/raw_ostream.h"
+#include <fstream>
 
 using namespace eld;
 
@@ -66,3 +67,117 @@ void Timer::print(llvm::raw_ostream &Os) {
 void Timer::printVal(double Val, llvm::raw_ostream &Os) {
   Os << llvm::format("  %7.4f ", Val);
 }
+
+llvm::MapVector<
+    llvm::StringRef,
+    llvm::MapVector<llvm::StringRef, RegisterTimer::MemoryUsageInfo>>
+    RegisterTimer::DiffMemoryInfo;
+llvm::MapVector<
+    llvm::StringRef,
+    llvm::MapVector<llvm::StringRef, RegisterTimer::MemoryUsageInfo>>
+    RegisterTimer::AbsMemoryInfo;
+
+RegisterTimer::RegisterTimer(llvm::StringRef name, llvm::StringRef group,
+                             bool enable)
+    : NamedRegionTimer(name, name, group, group, enable), Name(name),
+      Group(group) {
+  if (!shouldRecordMemory())
+    return;
+  StartMemInfo = getMemoryUsageInfo();
+  // Required to preserve the order!
+  DiffMemoryInfo[Group];
+}
+
+RegisterTimer::~RegisterTimer() {
+  if (!shouldRecordMemory())
+    return;
+  MemoryUsageInfo curMemInfo = getMemoryUsageInfo();
+  DiffMemoryInfo[Group][Name] += curMemInfo - StartMemInfo;
+  AbsMemoryInfo[Group][Name] = curMemInfo;
+  // llvm::errs() << "Storing memory info for " << Group << ":" << Name << "\n";
+}
+
+#ifdef __linux__
+bool RegisterTimer::ShouldRecordMemoryStats = false;
+
+RegisterTimer::MemoryUsageInfo RegisterTimer::getMemoryUsageInfo() {
+  std::ifstream statusFile("/proc/self/status");
+  std::stringstream content;
+  content << statusFile.rdbuf();
+  std::string line;
+  MemoryUsageInfo memInfo;
+  while (std::getline(content, line)) {
+    std::size_t pos = line.find(":");
+    if (pos == std::string::npos)
+      continue;
+    std::string key = line.substr(0, pos);
+    std::string valueStr = line.substr(pos + 1);
+    llvm::StringRef valueStrRef = valueStr;
+    valueStrRef = valueStrRef.trim();
+    valueStrRef.consume_back_insensitive(" kb");
+    if (key == "VmHWM") {
+      valueStrRef.getAsInteger(/*Radix=*/10, memInfo.RSSPeak);
+    } else if (key == "VmRSS") {
+      valueStrRef.getAsInteger(/*Radix=*/10, memInfo.RSSCur);
+    }
+  }
+  return memInfo;
+}
+
+void RegisterTimer::emitMemoryStats(llvm::raw_ostream &OS) {
+  for (auto &group : DiffMemoryInfo) {
+    llvm::StringRef groupName = group.first;
+    // llvm::errs() << "Writing memory stats for group: " << groupName << "\n";
+    const auto &groupMembersMemInfo = group.second;
+    OS << "===" << std::string(73, '-') << "===" << "\n";
+    unsigned padding = (80 - groupName.size()) / 2;
+    if (padding > 80)
+      padding = 0;
+    OS.indent(padding);
+    OS << groupName << "\n";
+    OS << "===" << std::string(73, '-') << "===" << "\n";
+    MemoryUsageInfo groupMemInfo = getGroupMemoryUsageInfo(groupName);
+    OS << "Total resident set size change: "
+       << groupMemInfo.getHumanReadableRSSCur() << "\n";
+    OS << "\n";
+    OS << "   ------RSS------";
+    OS << "   -RSS peak-";
+    OS << "   ---Name---";
+    OS << "\n";
+    for (const auto &elem : groupMembersMemInfo) {
+      llvm::StringRef name = elem.first;
+      const auto &memInfo = elem.second;
+      const auto &absMemInfo = AbsMemoryInfo[groupName][name];
+      OS << "   "
+         << llvm::format("%6s(+%6s)",
+                         absMemInfo.getHumanReadableRSSCur().c_str(),
+                         memInfo.getHumanReadableRSSCur().c_str());
+      OS << std::string(3, ' ')
+         << llvm::format("%6s", absMemInfo.getHumanReadableRSSPeak().c_str());
+      OS << std::string(7, ' ') << name << "\n";
+    }
+    OS << "\n\n";
+  }
+}
+
+bool RegisterTimer::shouldRecordMemory() const {
+  if (!ShouldRecordMemoryStats)
+    return false;
+  // All these timers are not suitable for memory tracking because they are
+  // triggered large number of times.
+  if (Group == "Symbol Resolution" || Name == "VisitSymbol" ||
+      Name == "VisitSections" || Name == "Sort Sections" ||
+      Name == "Evaluate Expressions")
+    return false;
+  return true;
+}
+
+RegisterTimer::MemoryUsageInfo
+RegisterTimer::getGroupMemoryUsageInfo(llvm::StringRef groupName) {
+  MemoryUsageInfo memInfo;
+  for (const auto &elem : DiffMemoryInfo[groupName]) {
+    memInfo += elem.second;
+  }
+  return memInfo;
+}
+#endif
