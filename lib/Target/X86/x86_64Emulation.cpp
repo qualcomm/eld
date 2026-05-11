cd0a4ddbaf9f6fe52703120afb8e9abf1c935c30
deepakshirkem <deepakshirke509@gmail.com>
2026-05-11 19:31:46 +0530

    [linker-script] Find namespec libraries in sysroot standard dirs
    ELDEmulateELF only searched =/lib and =/usr/lib under sysroot, missing
    =/lib64, =/usr/lib64, =/usr/local/lib and =/usr/local/lib64. Also,
    when no target is known upfront, initEmulator is deferred until the
    first ELF object is read, causing GROUP(-lfoo) namespec search to fail
    before any sysroot standard dirs are added.
    
    Fixes #819.
    
    Signed-off-by: deepakshirkem <deepakshirke509@gmail.com>

--- lib/Target/X86/x86_64Emulation.cpp
+++ lib/Target/X86/x86_64Emulation.cpp
@@ -3,14 +3,15 @@
 // See https://github.com/qualcomm/eld/LICENSE.txt for license information.
 // SPDX-License-Identifier: BSD-3-Clause
 //===----------------------------------------------------------------------===//
-
 //===----------------------------------------------------------------------===//
 #include "eld/Config/LinkerConfig.h"
 #include "eld/Core/LinkerScript.h"
+#include "eld/Input/ELDDirectory.h"
 #include "eld/Support/TargetRegistry.h"
 #include "eld/Target/ELFEmulation.h"
 #include "x86_64.h"
 #include "llvm/ADT/StringSwitch.h"
+#include "llvm/Support/FileSystem.h"
 
 using namespace llvm;
 
@@ -24,6 +25,29 @@ static bool ELDEmulatex86_64ELF(LinkerScript &pScript, LinkerConfig &pConfig) {
   if (!ELDEmulateELF(pScript, pConfig))
     return false;
 
+  if (pConfig.options().nostdlib())
+    return true;
+
+  auto Sysroot = pConfig.directories().hasSysRoot()
+                     ? pConfig.directories().sysroot().native()
+                     : "";
+
+  static const char *X86_64Dirs[] = {
+      "=/usr/local/lib/x86_64-linux-gnu",
+      "=/lib/x86_64-linux-gnu",
+      "=/usr/lib/x86_64-linux-gnu",
+      "=/usr/lib/x86_64-linux-gnu64",
+      "=/usr/local/lib64",
+      "=/lib64",
+      "=/usr/lib64",
+      "=/usr/x86_64-linux-gnu/lib64",
+      "=/usr/x86_64-linux-gnu/lib",
+  };
+  for (const char *Dir : X86_64Dirs) {
+    auto EldDir = ELDDirectory(Dir, Sysroot);
+    if (llvm::sys::fs::is_directory(EldDir.name()))
+      pConfig.directories().insert(Dir);
+  }
   return true;
 }
 
